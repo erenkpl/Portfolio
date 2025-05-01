132e0122dca8b0e500605db1ed8b2481dc27dbbc
Eren Küpeli <eren_kpl@hotmail.com>
2025-05-01 12:18:51 +0300

    nav bar update

--- js/index-script.js
+++ js/index-script.js
@@ -1,4 +1,7 @@
 function toggleMenu(){
     const menu = document.querySelector('.mobile-nav');
+    const hamburger = document.querySelector('.hamburger-menu');
+
     menu.classList.toggle('show');
+    hamburger.classList.toggle('active');
 }
