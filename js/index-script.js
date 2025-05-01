57562b664e9c5c4a2a5662ca686f4c1648a6c810
Eren Küpeli <eren_kpl@hotmail.com>
2025-05-01 13:00:11 +0300

    animation update

--- js/index-script.js
+++ js/index-script.js
@@ -1,3 +1,17 @@
+window.addEventListener('DOMContentLoaded', function(){
+    const foto = this.document.querySelector('.div-foto');
+    const yazi = this.document.querySelector('.yazi');
+
+    setTimeout(() => {
+        foto.classList.add('show');
+    }, 400);
+
+    setTimeout(() => {
+        yazi.classList.add('show');
+    }, 1000);
+
+});
+
 function toggleMenu(){
     const menu = document.querySelector('.mobile-nav');
     const hamburger = document.querySelector('.hamburger-menu');
